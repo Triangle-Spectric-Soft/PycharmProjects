ea68aaef7ec61d31a24b2bac313ca7205e0fe709
crvnk <imlequekirill@gmail.com>
2023-05-10 14:37:47 +0300

    lab1

---
+++ lab_2_Chepurnov.py
@@ -0,0 +1,21 @@
+# Перше завдання
+a1 = "Programming"
+a2 = "Essentials"
+a3 = "in...Python"
+result = a1 + "***" + a2 + "***" + a3
+print(result)
+
+#Друге завдання
+print("         *\n        * *\n       *   *\n      *     *\n     ***   ***\n       *   *\n       *   *\n       *****")
+
+#Третє завдання
+print("I'm student")
+
+#Четверте завдання
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+
+#П'яте завдання
+print(0o500)
+
+#Шосте завдання
+print(0x777)
